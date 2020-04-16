90eb84e5296273393c4691ebd3cbc9452b642dfd
Teague Henry <trhenry@live.unc.edu>
2020-04-16 17:58:44 -0400

    Update db.py

--- poefixer/db.py
+++ poefixer/db.py
@@ -307,9 +307,9 @@ class PoeDb:
         for field in simple_fields:
             setattr(row, field, getattr(thing, field, None))
         if table is Item:
-            if "Cluster Jewel" in thing._data["typeLine"]:
-                print("Got A Cluster Jewel")
-                self.session.add(row)
+           # if "Cluster Jewel" in thing._data["typeLine"]:
+            print("Got A Cluster Jewel")
+            self.session.add(row)
         return row
 
     @property
